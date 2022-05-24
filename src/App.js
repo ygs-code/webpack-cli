031bf0c2f97316a09946d77be15d271899c1dcd8
姚观寿 <yaoguanshou@rainbowcn.com>
2022-05-24 14:58:21 +0800

    添加json-loader和抽离css image等功能

--- src/App.js
+++ src/App.js
@@ -2,38 +2,41 @@
  * @Date: 2022-04-24 11:14:32
  * @Author: Yao guan shou
  * @LastEditors: Yao guan shou
- * @LastEditTime: 2022-05-23 20:54:58
+ * @LastEditTime: 2022-05-24 14:20:52
  * @FilePath: /webpack-cli/src/App.js
  * @Description:
  */
-import React, { Component } from "react";
-import { Provider } from "react-redux";
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
 // import Router from "./router/index";
-import store from "./redux/store";
-import Home from "@/pages/home";
-import "./App.css";
-import "./App1.less";
+import store from './redux/store'
+import Home from '@/pages/home'
+import json from './data.json'
+import './App.css'
+import './App1.less'
 // import "./App2.sass";
-import "./App3.scss";
+import './App3.scss'
 // import "./index.css";
 // asdfdsf
-console.log("process=", process); // 'test' 
-/*global APP_CONFIG*/ 
-var server_api_key = APP_CONFIG.api_key;
- 
-console.log( " APP_CONFIG=", APP_CONFIG);
-console.log("process=", process); 
+console.log('process=', process) // 'test'
+/*global APP_CONFIG*/
 
-//    
-//  
-//  pppp 
-    // 阿斯顿发  
+var server_api_key = APP_CONFIG.api_key
+
+console.log(' APP_CONFIG=', APP_CONFIG)
+console.log('process=', process)
+console.log('json=', json)
+
+//
+//
+//  pppp
+// 阿斯顿发
 const App = () => {
   return (
     <Provider store={store}>
       <Home />
     </Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@
  * @Date: 2022-04-24 11:14:59
  * @Author: Yao guan shou
  * @LastEditors: Yao guan shou
- * @LastEditTime: 2022-05-13 16:18:43
+ * @LastEditTime: 2022-05-24 13:41:59
  * @FilePath: /webpack-cli/src/pages/home/index.js
  * @Description:
  */
@@ -15,16 +15,25 @@ import React, {
   Children,
   useEffect,
   memo,
-} from "react";
+} from 'react'
 // import "./index.less";
-
+import img from '../../assets/image/react.jpg'
+import home from '../../assets/image/home.svg'
 
 // 权限跳转登录页面可以在这控制
 const Index = () => {
   // 阿斯顿发
-  return <div className="home">HOME页面</div>;
-};
+  return (
+    <div className="home">
+      HOME页面
+      <img src={img} />
+      <img
+      src={home}
+    />
+    </div>
+  )
+}
 
 // hhhrh
 
-export default Index;
+export default Index
