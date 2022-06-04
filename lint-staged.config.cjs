81234e21cdd87292155b7bf44c58bb244fc66832
yaoguanshou <281113270@qq.com>
2022-06-04 22:57:39 +0800

    999

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     'src/**/*.{js,jsx,ts,tsx}': [
         'prettier --tab-width 4 --write',
-        'eslint src/**/*.js  --fix',
+        'eslint  --fix',
         'git add',
     ],
     '*.{htm,html,css,sss,less,scss,saas}': ['stylelint --fix', 'git add'],
