d74264ed0cecefbb8fee5f3aac9a92a2dba54744
yaoguanshou <281113270@qq.com>
2022-06-04 19:53:46 +0800

    1

--- user-webpack-config/webpack.base.config.js
+++ user-webpack-config/webpack.base.config.js
@@ -1,14 +1,27 @@
 const webpack = require('webpack');
-const HtmllinterWebpackPlugin = require('htmllinter-webpack-plugin');
+const fs = require('fs');
 const htmllintLoader = require('htmllint-loader');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const ExtendedDefinePlugin = require('extended-define-webpack-plugin');
+const HtmllinterWebpackPlugin = require('htmllinter-webpack-plugin');
 const MyExampleWebpackPlugin = require('./definePlugin/MyExampleWebpackPlugin');
 const os = require('os');
 const HappyPack = require('happypack');
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length - 1 });
 const path = require('path');
+
+
+let htmllinterConfig = {};
+if (fs.existsSync(path.join(process.cwd(), './htmllinter.config.js'))) {
+    htmllinterConfig = require(path.join(
+        process.cwd(),
+        './htmllinter.config.js'
+    ));
+}
+
+console.log('htmllinterConfig=', htmllinterConfig);
+
 const { resolve } = path;
 let {
     NODE_ENV, // 环境参数
@@ -41,24 +54,24 @@ module.exports = {
         // loader
         rules: [
             //配置 https://github.com/robbiedigital/htmllint-loader
-            {
-                test: /\.(htm|html|xhtml|hbs|handlebars|php|ejs)$/,
-                // loader: path.join(
-                //     __dirname,
-                //     './defineLoader/MyExampleWebpackLoader.js'
-                // ),
-                loader: 'htmllint-loader',
-                exclude: /(node_modules)/,
-                options: {
-                    // config: path.join(process.cwd(), '/.htmllintrc'),
-                    //   config: '.htmllintrc', // path to custom config file
-                    emitError: true, //发现的错误将始终被触发，将禁用设置为false。
-                    emitWarning: true, //如果将disable设置为false，则发现的警告将始终被发出。
-                    failOnError: true, //如果有任何错误，将导致模块构建失败，禁用设置为false。
-                    failOnWarning: false, //如果有任何警告，如果设置为true，将导致模块构建失败。
-                    quiet: false, //如果设置为true，将只处理和报告错误，而忽略警告。
-                },
-            },
+            // {
+            //     test: /\.(htm|html|xhtml|hbs|handlebars|php|ejs)$/,
+            //     // loader: path.join(
+            //     //     __dirname,
+            //     //     './defineLoader/MyExampleWebpackLoader.js'
+            //     // ),
+            //     loader: 'htmllint-loader',
+            //     exclude: /(node_modules)/,
+            //     options: {
+            //         config: path.join(process.cwd(), '/.htmllintrc'),
+            //         //   config: '.htmllintrc', // path to custom config file
+            //         emitError: true, //发现的错误将始终被触发，将禁用设置为false。
+            //         emitWarning: true, //如果将disable设置为false，则发现的警告将始终被发出。
+            //         failOnError: true, //如果有任何错误，将导致模块构建失败，禁用设置为false。
+            //         failOnWarning: false, //如果有任何警告，如果设置为true，将导致模块构建失败。
+            //         quiet: false, //如果设置为true，将只处理和报告错误，而忽略警告。
+            //     },
+            // },
             {
                 test: /\.js?$/,
                 enforce: 'pre',
@@ -78,27 +91,27 @@ module.exports = {
     plugins: [
         // // he
         new HtmllinterWebpackPlugin({
-            config: {
-                //   extend: require('@htmllinter/basic-config'),
-
-                rules: {
-                    'no-empty-tag': 'on',
-                    'no-duplicate-id': 'on',
-                    'no-duplicate-tag': 'on',
-                    'no-bool-true-explicit-define': 'on',
-                    'doctype-top': [
-                        'on',
-                        {
-                            startingLineNumber: 1,
-                        },
-                    ],
-                    'long-line-content': 'on',
-                    'no-missing-alt': 'on',
-                    'newline-multiline-comments': 'on',
-                    'trailing-singleline-comments': 'on',
-                    'newline-eof': 'on',
-                },
-            },
+            config: htmllinterConfig,
+            // config: {
+            //     extend: require('@htmllinter/basic-config'),
+            //     rules: {
+            //         'no-empty-tag': 'on',
+            //         'no-duplicate-id': 'on',
+            //         'no-duplicate-tag': 'on',
+            //         'no-bool-true-explicit-define': 'on',
+            //         'doctype-top': [
+            //             'on',
+            //             {
+            //                 startingLineNumber: 1,
+            //             },
+            //         ],
+            //         'long-line-content': 'on',
+            //         'no-missing-alt': 'on',
+            //         'newline-multiline-comments': 'on',
+            //         'trailing-singleline-comments': 'on',
+            //         'newline-eof': 'on',
+            //     },
+            // },
         }),
         // stylelint 插件
         new StylelintPlugin({
