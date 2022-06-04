42a6136b11e8dfb2763a6d7ef3227367a112fd7c
yaoguanshou <281113270@qq.com>
2022-06-04 23:20:48 +0800

    修复bug

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
     'src/**/*.{js,jsx,ts,tsx}': [
         'prettier --tab-width 4 --write',
-        'eslint',
+        'eslint  --fix',
         'git add',
     ],
-    'src/**/*.{htm,html,css,sss,less,scss,saas}': ['stylelint', 'git add'],
+    'src/**/*.{htm,html,css,sss,less,scss,saas}': ['stylelint --fix', 'git add'],
 };
