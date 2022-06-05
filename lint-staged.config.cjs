765e5a6005d27bb7a11e77dd8c533a859f96f9d6
yao guan shou <281113270@qq.com>
2022-06-05 20:05:27 +0800

    feat: ghbjkhhgkjh

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,7 +2,6 @@ module.exports = {
     'src/**/*.{js,jsx,ts,tsx}': [
         'prettier --tab-width 3 --write',
         'eslint  --fix',
-        'git add',
     ],
-    'src/**/*.{htm,html,css,sss,less,scss,saas}': ['stylelint --fix', 'git add'],
+    'src/**/*.{htm,html,css,sss,less,scss,saas}': ['stylelint --fix'],
 };
