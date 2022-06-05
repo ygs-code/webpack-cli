f1065341684acf41f8d3a598d5cb1fe2364ac1f1
yaoguanshou <281113270@qq.com>
2022-06-05 12:21:35 +0800

    feat: 增加 xxx 功能

---
+++ bin/git.js
@@ -0,0 +1,74 @@
+const inquirer = require('inquirer'); // 与用户互动
+const execSync = require('child_process').execSync;
+const addReg = /git add/gi;
+const pushReg = /git push/gi;
+const committedReg = /committed/gi;
+
+// console.log('git push'.match(pushReg));
+// inquirer
+//     .prompt([
+//         { commit: 'git', type: 'input', message: '请输入commit信息' },
+//     ])
+//     .then((answer) => {
+//         console.log(answer);
+//     });
+
+// inquirer
+//     .prompt([
+//         {
+//             name: 'wants_pizza',
+//             type: 'confirm',
+//             message: 'Do you want a free pizza?',
+//         },
+//     ])
+//     .then((answer) => {
+//         console.log(answer.wants_pizza);
+//     });
+
+const gitPush = async () => {
+    let { isSubmit } = await inquirer.prompt([
+        {
+            name: 'isSubmit',
+            type: 'confirm',
+            message: '确定提交代码么？',
+        },
+    ]);
+    if (!isSubmit) {
+        return false;
+    }
+    const status = execSync('git status').toString();
+    // console.log('status=', status);
+    // console.log('status.match(addReg)=', status.match(addReg));
+
+    if (status.match(addReg)) {
+        const add = execSync('git add .');
+        console.log('add=', add);
+    }
+
+    if (status.match(committedReg)) {
+        let { commitMessage } = await inquirer.prompt([
+            {
+                name: 'commitMessage',
+                type: 'input',
+                message: '请输入commit信息',
+            },
+        ]);
+    }
+
+    if (status.match(pushReg)) {
+        const push = execSync('git push');
+        console.log('push=', push);
+    }
+
+    // const add = execSync('git add .');
+    // console.log('add=', add.toString());
+    // let { commitMessage } = await inquirer.prompt([
+    //     { name: 'commitMessage', type: 'input', message: '请输入commit信息' },
+    // ]);
+
+    // .then((answer) => {
+    //     console.log(answer);
+    // });
+};
+
+gitPush();
