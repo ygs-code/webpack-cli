366c25645dc0b15a33fbfeedc7fcb54d3e858eb6
yao guan shou <281113270@qq.com>
2022-06-05 20:07:49 +0800

    feat: 你好

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ console.log("process ==", process); // 'test'
 var server_api_key = APP_CONFIG.api_key;
 console.log("APP_CONFIG=", APP_CONFIG);
 
-console.log("process=", process);
+console.log("process=  ", process);
 // 撒地方
 // 阿斯顿发
 const App = () => {
