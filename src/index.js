3cbe354756152710814d2b989d62fe069570b76a
姚观寿 <yaoguanshou@rainbowcn.com>
2022-07-04 16:45:13 +0800

    build: 修改热刷新插件

--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@
  * @Date: 2022-04-24 11:14:20
  * @Author: Yao guan shou
  * @LastEditors: Yao guan shou
- * @LastEditTime: 2022-07-04 16:39:41
+ * @LastEditTime: 2022-07-04 16:44:42
  * @FilePath: /webpack-cli/src/index.js
  * @Description:
  */
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom';
-import {createRoot} from 'react-dom/client';
-import './App.css';
-import App from './App';
+import {createRoot} from "react-dom/client";
+import "./App.css";
+import App from "./App";
 let a = 123;
 // sdf;
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
